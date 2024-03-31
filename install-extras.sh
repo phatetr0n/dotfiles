da674f5e5988404b32e7029b844708fabcc7d6ef
phatetr0n <git@aaltonen.ca>
2024-03-30 20:46:58 -0400

    add install script for extra packages

---
+++ install-extras.sh
@@ -0,0 +1,5 @@
+# install kitty, zsh, and plugins
+sudo pacman -S kitty zsh zsh-autosuggestions zsh-syntax-highlighting
+
+# install oh-my-zsh and powerlevel10k for customization of kitty. Also pokemon
+yay -S oh-my-zsh-git zsh-theme-powerlevel10k pokemon-colorscripts-git
