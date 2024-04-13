480ec04f0f07d95c54587d276709947b99e49cf9
phatetr0n <git@aaltonen.ca>
2024-04-13 15:37:59 -0400

    fixed install script, oops

--- install-extras.sh
+++ install-extras.sh
@@ -5,6 +5,6 @@ sudo pacman -S kitty zsh zsh-autosuggestions zsh-syntax-highlighting
 yay -S oh-my-zsh-git zsh-theme-powerlevel10k pokemon-colorscripts-git
 
 # clone oh-my-zsh plugins into their proper place
-git clone https://github.com/chrissicool/zsh-256color ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-256color # zsh-256color
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions # zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting # zsh-syntax-highlighting
+git clone https://github.com/chrissicool/zsh-256color /usr/share/oh-my-zsh/plugins/custom/plugins/zsh-256color # zsh-256color
+git clone https://github.com/zsh-users/zsh-autosuggestions /usr/share/oh-my-zsh/plugins/custom/plugins/zsh-autosuggestions # zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git /usr/share/oh-my-zsh/plugins/custom/plugins/zsh-syntax-highlighting # zsh-syntax-highlighting
