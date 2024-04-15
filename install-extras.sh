a108c37c2c55903ce9de5334201a959b3990ef61
phatetr0n <git@aaltonen.ca>
2024-04-14 21:59:12 -0400

    added kitty link to script for automation

--- install-extras.sh
+++ install-extras.sh
@@ -8,3 +8,6 @@ yay -S oh-my-zsh-git zsh-theme-powerlevel10k pokemon-colorscripts-git
 sudo git clone https://github.com/chrissicool/zsh-256color /usr/share/oh-my-zsh/custom/plugins/zsh-256color # zsh-256color
 sudo git clone https://github.com/zsh-users/zsh-autosuggestions /usr/share/oh-my-zsh/custom/plugins/zsh-autosuggestions # zsh-autosuggestions
 sudo git clone https://github.com/zsh-users/zsh-syntax-highlighting.git /usr/share/oh-my-zsh/custom/plugins/zsh-syntax-highlighting # zsh-syntax-highlighting
+
+# link kitty config
+ln -s $HOME/dotfiles/kitty $HOME/.config/kitty
