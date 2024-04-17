e0df82ffd18aaa5b6efdec54b57a71b7026771c6
phate <phate@tr0n.ca>
2024-04-17 16:12:37 -0400

    repaired laptop install script to bring in line with master branch

--- install-extras.sh
+++ install-extras.sh
@@ -1,5 +1,13 @@
-# install kitty, zsh, and plugins
-sudo pacman -S kitty zsh zsh-autosuggestions zsh-syntax-highlighting
+# install kitty and zsh
+sudo pacman -S kitty zsh
 
 # install oh-my-zsh and powerlevel10k for customization of kitty. Also pokemon
 yay -S oh-my-zsh-git zsh-theme-powerlevel10k pokemon-colorscripts-git
+
+# clone oh-my-zsh plugins into their proper place
+sudo git clone https://github.com/chrissicool/zsh-256color /usr/share/oh-my-zsh/custom/plugins/zsh-256color # zsh-256color
+sudo git clone https://github.com/zsh-users/zsh-autosuggestions /usr/share/oh-my-zsh/custom/plugins/zsh-autosuggestions # zsh-autosuggestions
+sudo git clone https://github.com/zsh-users/zsh-syntax-highlighting.git /usr/share/oh-my-zsh/custom/plugins/zsh-syntax-highlighting # zsh-syntax-highlighting
+
+# link kitty config
+ln -s $HOME/dotfiles/kitty $HOME/.config/kitty
